db91bb450ccca9c52fc5a4bbf181117f6d307f12
Sven Kuehn <svku70@gmx.net>
2018-05-22 21:42:38 +0200

    palyed with images and srcsets

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-responsive-images');
   // Default task(s).
   grunt.registerTask('default', ['uglify','cssmin']);
 };
--- setup_grunt.sh
+++ setup_grunt.sh
@@ -12,6 +12,7 @@ fi
 #npm install grunt-contrib-cssmin --save-dev
 #npm install grunt-autoprefixer --save-dev
 #npm install grunt-contrib-copy --save-dev
+# see https://github.com/andismith/grunt-responsive-images/blob/master/README.md
 brew install GraphicsMagick
 brew install ImageMagick
 #npm install grunt-responsive-images --save-dev
