8f5c2dc04efcd697e5226a6337d26a646f57a234
Sven Kuehn <svku70@gmx.net>
2018-05-22 07:25:04 +0200

    some deploy scripts .ignore git file basic grunt file

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,1 +1,3 @@
 cp -Rfv ~/Udacity/FEND/web_foundations/FEND-web_foundation-final/ /usr/local/var/www/Udacity/Fend-web/
+sudo brew services stop httpd
+sudo brew services start httpd
---
+++ setup_grunt.sh
@@ -0,0 +1,17 @@
+if [ "$1" == "full" ]
+then
+    echo 'Full install'
+elif [ "$1" == "update" ]
+    then
+    echo "Update only"
+fi
+#npm update
+#npm install grunt-cli --save-dev
+#npm install grunt --save-dev
+#npm install grunt-contrib-uglify --save-dev
+#npm install grunt-contrib-cssmin --save-dev
+#npm install grunt-autoprefixer --save-dev
+#npm install grunt-contrib-copy --save-dev
+brew install GraphicsMagick
+brew install ImageMagick
+#npm install grunt-responsive-images --save-dev
